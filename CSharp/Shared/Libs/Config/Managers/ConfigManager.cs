7728b009acd5b9dd11846d627e12d75baf4fef52
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2025-11-08 07:47:43 +0300

    import from jovian radiation rework

--- CSharp/Shared/Libs/Config/Managers/ConfigManager.cs
+++ CSharp/Shared/Libs/Config/Managers/ConfigManager.cs
@@ -17,6 +17,8 @@ namespace BaroJunk
 
     public void UseStrategy(ConfigStrategy strategy)
     {
+      CurrentStrategy = strategy;
+
       AutoSaver.UseStrategy(strategy.AutoSaverStrategy);
 
       if (Config.Facades.NetFacade.IsClient)
@@ -28,6 +30,8 @@ namespace BaroJunk
         ServerNetManager.UseStrategy(strategy.NetManagerStrategy);
       }
     }
+
+    public ConfigStrategy CurrentStrategy;
 
     public ConfigAutoSaver AutoSaver;
     public ConfigClientNetManager ClientNetManager;
--- CSharp/Shared/Libs/ReflectionProxy.cs
+++ CSharp/Shared/Libs/ReflectionProxy.cs
@@ -11,22 +11,57 @@ using Microsoft.Xna.Framework;
 
 namespace BaroJunk
 {
+  /// <summary>
+  /// Wrapper around an object that adds Get Set methods
+  /// Accepts strings and parses them automatically
+  /// Made for console commands
+  /// </summary>
   public class ReflectionProxy
   {
+    public class NotAProp : Attribute { }
+
     public static BindingFlags pls = BindingFlags.Instance | BindingFlags.Public;
-    public object Target { get; private set; }
+    public object Target { get; set; }
 
-    public object Get(string prop) => Target.GetType().GetProperty(prop, pls)?.GetValue(Target);
-    public void Set(string prop, object value) => Target.GetType().GetProperty(prop, pls)?.SetValue(Target, value);
-    public bool Has(string prop) => Target.GetType().GetProperty(prop, pls) is not null;
-    public IEnumerable<string> Props => Target.GetType().GetProperties(pls).Select(pi => pi.Name);
+    public object Get(string prop) => GetInfo(prop)?.GetValue(Target);
 
-    public override string ToString() => Target.ToString();
+    public SimpleResult Set(string prop, string raw)
+    {
+      if (!Has(prop)) return SimpleResult.Failure("no such prop");
+      SimpleResult result = SimpleParser.Default.Parse(raw, TypeOf(prop));
+      if (result.Ok) GetInfo(prop)?.SetValue(Target, result.Result);
+      return result;
+    }
+
+    public bool Has(string prop) => GetInfo(prop) is not null;
+
+    public Type TypeOf(string prop)
+    {
+      PropertyInfo pi = GetInfo(prop);
+      if (pi is null) return null;
+      return pi.PropertyType;
+    }
+
+    public IEnumerable<string> GetPropNames()
+      => Target.GetType().GetProperties(pls)
+      .Where(pi => !Attribute.IsDefined(pi, typeof(NotAProp)))
+      .Select(pi => pi.Name);
+
+    public PropertyInfo GetInfo(string prop)
+    {
+      PropertyInfo pi = Target.GetType().GetProperty(prop, pls);
+      if (pi is null) return null;
+      if (Attribute.IsDefined(pi, typeof(NotAProp))) return null;
+      return pi;
+    }
+
     public ReflectionProxy(object target)
     {
       ArgumentNullException.ThrowIfNull(target);
       Target = target;
     }
+
+    public override string ToString() => Target.ToString();
   }
 
 }
