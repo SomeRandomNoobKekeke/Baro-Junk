22b7f5485d127a49ed0a4c2445d01fb578f3671d
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-05-07 17:58:37 +0300

    new clearable events

---
+++ CSharp/Shared/Libs/ClearableEvent/ClearableEvent.Variants4.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace BaroJunk
+{
+  public class ClearableEvent<T1, T2, T3, T4>
+  {
+    private event Action<T1, T2, T3, T4> Event;
+    public bool Empty => Event == null;
+    public event Action<Action<T1, T2, T3, T4>> OnSubscribed;
+    public event Action<Action<T1, T2, T3, T4>> OnUnSubscribed;
+    public EventSubscription Add(Action<T1, T2, T3, T4> callback)
+    {
+      Event += callback;
+      OnSubscribed?.Invoke(callback);
+      return new EventSubscription(() =>
+      {
+        Event -= callback;
+        OnUnSubscribed?.Invoke(callback);
+      });
+    }
+    public void Remove(Action<T1, T2, T3, T4> callback)
+    {
+      Event -= callback;
+      OnUnSubscribed?.Invoke(callback);
+    }
+    public void Raise(T1 arg1, T2 arg2, T3 arg3, T4 arg4) => Event?.Invoke(arg1, arg2, arg3, arg4);
+    public void Clear()
+    {
+      if (Event is null) return;
+
+      foreach (Delegate callback in Event.GetInvocationList())
+      {
+        Event -= (Action<T1, T2, T3, T4>)callback;
+      }
+    }
+  }
+}
---
+++ CSharp/Shared/Libs/DebugNode/DebugNode.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BaroJunk
+{
+  public class DebugNode
+  {
+
+  }
+}
