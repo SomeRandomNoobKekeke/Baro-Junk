c5fb25b8e845ee11b35d5679115e551ce27d95e9
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-05-08 17:55:45 +0300

    bruh

---
+++ CSharp/Shared/Libs/ClearableEvent/Test/ClearableEvent.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+using Barotrauma;
+
+
+namespace BaroJunk
+{
+
+  public class ClearableEventTest : UTestPack
+  {
+    public UTest ConceptTest()
+    {
+
+
+      return new UTest(123, 123);
+    }
+  }
+
+
+}
